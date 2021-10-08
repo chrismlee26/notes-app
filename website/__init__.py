13cfe656307783e184a70bef840ed59c1a58da07
CHRIS_LEE <chrismlee87@gmail.com>
2021-10-08 04:29:44 -0700

    Fix venv and dependancy issues wow

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask, app
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'random string'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'SQLITE:///{DB_NAME}'
+
+    db.init_app(app)
 
     # Routes
     from .views import views
--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,15 @@
 # authentication
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text="Testing", boolean=True)
+    # data = request.form
+    # print(data)
+    return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
@@ -15,6 +17,20 @@ def logout():
     return "<p>logout</p>"
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        nickname = request.form.get('nickname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 6:
+            flash('That\'s no real email bro', category='error')
+        elif len(nickname) < 2:
+            flash('You need more than 2 characters', category='error')
+        elif password1 >= password2:
+            flash('passwords should match', category='error')
+        elif len(password1) > 7:
+            flash('Password should be longer than 7 characters', category='error')
     return render_template("sign_up.html")
