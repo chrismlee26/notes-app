ffd8ad15f2427f51e90a6cd86c2c293860da2358
CHRIS_LEE <chrismlee87@gmail.com>
2021-10-08 08:36:46 -0700

    Fix lignup login fully working

--- website/auth.py
+++ website/auth.py
@@ -10,6 +10,18 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Ya did it! (logged in)', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email doens\'t exist', category='error')
     return render_template("login.html", boolean=True)
 
 
@@ -26,7 +38,10 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 6:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+        elif len(email) < 6:
             flash('That\'s no real email bro', category='error')
         elif len(nickname) < 2:
             flash('You need more than 2 characters', category='error')
@@ -41,5 +56,4 @@ def signup():
             db.session.commit()
             flash('Welcome, account created :O', category='success')
             return redirect(url_for('views.home'))
-
     return render_template("sign_up.html")
